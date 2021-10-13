116c5808b3363a05bd1fbb5aa4c571e371997578
DELL <DELL@DESKTOP-OGRVV23>
2021-10-13 14:27:36 +0700

    baitapham3

---
+++ src/baitapham3/Baitapham3.java
@@ -0,0 +1,43 @@
+
+package baitapham3;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+public class Baitapham3 {
+
+   
+       
+
+   
+	public static void cau2() {
+	  int s,i;
+	     do 	{  
+		  Scanner scaner = new Scanner(System.in);
+	        System.out.println("Nhap n" );
+	        s = scaner.nextInt();
+	     }
+	        while (s < 0 ) ;
+	        {
+	        	for (i = 1 ; i > 0 ; i++)
+	        	{
+	        	System.out.println(s);
+	        	}
+	        }
+	      
+	        
+	        	
+	
+	
+}
+	private static Object i(int i, int j) {
+		
+		return null;
+	}
+	public static void main(String[] args) {
+		
+		cau2();
+	}
+}
+    
+    
+
